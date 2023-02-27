6bf0ee99bc13abe59b24e5d8b8fae656c5ba9844
Dmitree <color.ivanov2016@yandex.ru>
2023-02-27 21:09:21 +0300

    initial commit

---
+++ src/main/java/pet/project/javaworkforyou/JavaWorkForYouApplication.java
@@ -0,0 +1,13 @@
+package pet.project.javaworkforyou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaWorkForYouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaWorkForYouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pet/project/javaworkforyou/JavaWorkForYouApplicationTests.java
@@ -0,0 +1,13 @@
+package pet.project.javaworkforyou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaWorkForYouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
